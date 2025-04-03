3baea48c86403a18570e987a2b219b927855774d
jiyanbin <jiyanbin@021.com>
2025-04-03 11:18:01 +0800

    fix

---
+++ server.ts
@@ -0,0 +1,23 @@
+import { readFileSync } from 'fs';
+import Fastify from 'fastify';
+import consoleLogger from './console-logger.js';
+const fastify = Fastify({
+  logger: true,
+});
+let loggerViewerHtml = readFileSync('./logger-viewer.html').toString();
+fastify.get('/logger-viewer.html', async function handler(req, res) {
+  res.headers({
+    'Content-Type': 'text/html',
+  });
+  return loggerViewerHtml;
+});
+fastify.register(consoleLogger);
+// Run the server!
+(async () => {
+  try {
+    await fastify.listen({ port: 3000 });
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+})();
