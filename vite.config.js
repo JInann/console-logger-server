4a2db6896f0159a3eeb7a6ead017234112d8ce3b
jiyanbin <jiyanbin@021.com>
2025-04-07 16:15:50 +0800

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-
+import viteConsoleLogger from 'vite-plugin-console-logger';
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  root: 'web',
+  plugins: [vue(), viteConsoleLogger({ onlyServer: true })],
+  server: {
+    port: 8080,
+  },
 });
